9284ff2a4afb43f2e3ea1ed2f3b5539b401d6733
Sergey Molchanov <smol4anov@yandex.ru>
2022-05-17 13:39:34 +0300

    fix: fixed after first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-let popup = document.querySelector('.popup');
-let profileElement = document.querySelector('.profile');
+const popup = document.querySelector('.popup');
+const profileElement = document.querySelector('.profile');
 
-let title = profileElement.querySelector('.profile__title');
-let subtitle = profileElement.querySelector('.profile__subtitle');
-let editButton = profileElement.querySelector('.profile__edit-button');
+const title = profileElement.querySelector('.profile__title');
+const subtitle = profileElement.querySelector('.profile__subtitle');
+const editButton = profileElement.querySelector('.profile__edit-button');
 
-let popupForm = popup.querySelector('.popup__conteiner');
-let nameInput = popup.querySelector('#name');
-let aboutInput = popup.querySelector('#about-self');
-let closeButton = popup.querySelector('.popup__close');
+const popupForm = popup.querySelector('.popup__conteiner');
+const nameInput = popup.querySelector('#name');
+const aboutInput = popup.querySelector('#about-self');
+const closeButton = popup.querySelector('.popup__close');
 
 function showPopup() {
   popup.classList.add('popup_opened');
